a05d370e24f58d82e7306d9f457ffc8a6b50e11c
truong2595 <truong2592005@gmail.com>
2026-06-03 22:54:45 +0700

    feat: thêm dev_bme280 hwi2c dev_lcd_i2c
    - Tách file bme280_lcd_i2c thành 3 file cho i2c, lcd, bme280

---
+++ Core/Src/dev_bme280.c
@@ -0,0 +1,8 @@
+/*
+ * dev_bme280.c
+ *
+ *  Created on: Jun 3, 2026
+ *      Author: laptop hp
+ */
+
+
---
+++ Core/Src/dev_lcd_i2c.c
@@ -0,0 +1,8 @@
+/*
+ * dev_lcd_i2c.c
+ *
+ *  Created on: Jun 3, 2026
+ *      Author: laptop hp
+ */
+
+
